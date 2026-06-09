703a3a95c552fcfe375537806851b0af106a66b4
jramos-byte <jramos2025@alu.uct.cl>
2026-06-09 18:58:01 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -283,3 +283,97 @@ se desarrollo esta aplicacion esta diseñada para :
 #======================================
 #LIMITE INFINITO
 #======================================
+    def crear_infinito(self):
+        contenedor = ctk.CTkFrame(self.tab_infinito)
+        contenedor.pack(fill="both", expand=True, padx=10, pady=10)
+
+        menu_datos = ctk.CTkFrame(contenedor, width=200, height=200)
+        menu_datos.pack(side="bottom", fill="both", padx=10, pady=10)
+
+        grafica = ctk.CTkFrame(contenedor)
+        grafica.pack(side="top", fill="both", expand=True)
+
+        ctk.CTkLabel(menu_datos, text="limites al infinito", font=("arial", 22, "bold")).pack(pady=5)
+
+        ctk.CTkLabel(menu_datos, text="tendencia (∞ o -∞):").pack()
+        self.x_inf = ctk.CTkEntry(menu_datos, placeholder_text="oo o -oo")
+        self.x_inf.pack(pady=5)
+
+        ctk.CTkLabel(menu_datos, text="f(x):").pack()
+        self.fx_inf = ctk.CTkEntry(menu_datos, placeholder_text="Ej: (2*x)/x")
+        self.fx_inf.pack(pady=5)
+
+        ctk.CTkLabel(menu_datos, text="Explicación:").pack(pady=2)
+        self.caja_explicacion_inf = ctk.CTkTextbox(menu_datos, width=700, height=80, font=("arial", 13))
+        self.caja_explicacion_inf.pack(pady=5)
+        self.caja_explicacion_inf.configure(state="disabled")
+
+        self.figura_inf, self.eje_inf, self.canvas_inf = self.crea_grafico(grafica)
+    
+        ctk.CTkButton(
+            menu_datos,
+            text="calcular y graficar",
+            command=self.graficar_infinito
+        ).pack(pady=10)
+
+        def graficar_infinito(self):
+            try:
+                # Captura de datos desde la interfaz
+                tendencia = self.x_inf.get() #Rescata el texto de la tendencia (oo o -oo)
+                funcion = self.fx_inf.get() #rescata el string de la funcion ingresada
+
+                #Validacion de datos incompletos 
+                if not tendencia or not funcion: 
+                    self.mostrar_texto(self.caja_explicacion_inf, "ingresa los datos.")
+                    return #corta la ejecucion si falta alfun dato
+                x = sp.symbols('x')#Define 'x' como la variable simbolica pra el calculo
+
+                #traducimos la entrada de texto al objeto infinito de SymPy
+                if tendencia == "oo":
+                    tendencia_trad = sp.oo #asigna el objeto de infinito poitivo "es una dolbe 'o'"
+                elif tendencia == "-oo":
+                    tendencia_trad = -sp.oo # se asigna el mismo objeto pero con un "-" antes
+                else: 
+                    #si el usuario escribe cyalquier otra cosa, avisa del error y detiene el proceso
+                    self.mostrar_texto(self.caja_explicacion_inf, "Para infinito usa exactamente 'oo' o '-oo'.")
+                    return
+                fx = sp.sympify(funcion) #traduce el string de la funcion a una exprecion matematica
+                resultado = sp.limit(fx, x, tendencia_trad) # calcula el limite exacto
+
+                # Graficamos un rango amplio fijo (de -50 a 50) para apreciar el comportamiento asintotico 
+                puntos_x = []
+                puntos_y = []
+                for i in range(-50,51):
+                    try:
+                        valor_y = float(fx.subs(x, i).evalf()) # Remplaza 'x' por el valor de 'i' y lo evalua decimal
+                        if abs(valor_y) < 100: #acota los valores para evitar desbordes por asintotas verticales 
+                            puntos_x.append(i) #guarda el valor en x si pasa el flitro
+                            puntos_y.append(valor_y) #guarda el valor calculado de y
+                    except:
+                        continue # si hay una division por cero en ese punto, ignora el error y salta al siguiente 
+                    # Renderiza el grafico en matplotlib
+                self.eje_inf.clear()
+                self.eje_inf.plot(puntos_x, puntos_y, label=f"f(x) = {funcion}", color="#1500ff",linewidth=2) # Dibuja la curva principar de la funcion uniendo los puntos con una linea azul
+
+                # si el limite da un numero estable, se dibuja la asintota horizontal
+                if resultado.is_number:
+                    asintota = float(resultado.evalf()) #convierte el resultado del el limite a un valor decimal flotante 
+                    self.eje_inf.axhline(asintota, color='red', linestyle=':', label=f"Asintota y = {resultado}") # traza una linea horiontal punteada de color rojo en el valor donde convegue la funcion
+
+                self.eje_inf.axhline(0, color='white', linewidth=0.5) #dibuja el eje x
+                self.eje_inf.axvline(0, color='white', linewidth=0.5) #dibuja el eje y
+                self.eje_inf.grid(True, linestyle=':', color='gray', alpha=0.5)# activa una cuadricula gris 
+                self.eje_inf.legend(loc="upper right") #coloca el cuadro de etiquetas/leyendas arriba a la derecha 
+                self.canvas_inf.draw() #refresca fisicamente el liezo de la interfaz grafica
+            
+                #envia el resultado textual de la operacion limpia a la caja de texto
+                self.mostrar_texto(self.caja_explicacion_inf, f"Analizando comportamiento en tendencias extremas hacia {tendencia}. \nResultado del limite:{resultado}")
+            except Exception as error:
+                #si el usuario ingresa un error de sintaxisa se captura la exepcion y la muestra 
+                self.mostrar_texto(self.caja_explicacion_inf, f"Error:\n{str(error)}")
+
+
+        
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
